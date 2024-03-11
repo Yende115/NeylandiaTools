becaf8e7b3a55ca10119c00d1bc5cfb948b26e19
Yende <43997918+Yende115@users.noreply.github.com>
2024-03-11 22:51:27 +0100

    Add a listener for changing the minecart speed
    The minecarts are now going faster on powered rails.

--- src/main/java/ch/yende/neylandiatools/listeners/ListenerMinecartSpeed.java
+++ src/main/java/ch/yende/neylandiatools/listeners/ListenerMinecartSpeed.java
@@ -21,13 +21,18 @@ public class ListenerMinecartSpeed implements Listener {
 
     @EventHandler
     public void onVehicleMove(VehicleMoveEvent event) {
+
+        //Si le véhicule est un minecart
         if (event.getVehicle() instanceof Minecart) {
+
+            //Création des variables
             Minecart minecart = (Minecart) event.getVehicle();
             Block blockUnder = minecart.getLocation().getBlock().getRelative(0, 0, 0);
 
-            if (blockUnder.getType() == Material.RAIL) {
+            //Si le bloc en-dessous du minecart est un rail
+            if (blockUnder.getType() == Material.POWERED_RAIL) {
                 Vector currentVelocity = minecart.getVelocity();
-                double speedMultiplier = 10;
+                double speedMultiplier = 1.2;
 
                 Vector newVelocity = currentVelocity.multiply(speedMultiplier);
                 minecart.setVelocity(newVelocity);
